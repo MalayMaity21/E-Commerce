c3254a78f40051815b8b8642a8789db219db92c8
rimanshupatel <rimanshupatel1@gmail.com>
2025-03-06 18:31:12 +0530

    Adding Home carousel , Fixing HomePage design, Footer icons

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HomeBanner from "../components/ui/HomeBanner";
 import Categories from "../components/ui/Categories";
-
+import Recommendations from "../components/ui/Recommendation";
 const HomePage = () => {
   return (
     <>
       <HomeBanner />
       <Categories />
+      <Recommendations />
     </>
   );
 };
